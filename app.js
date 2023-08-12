1d4c0dfebfe83b7326752887e8a363b64985614a
Ji Young Do <dojiyoung1844@gmail.com>
2023-08-13 00:36:14 +1200

    added export the Express API

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const books = require('./routes/api/books');
 const app = express();
 
 // Connect Database
-connectDB();
+//connectDB();
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
