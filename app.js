49187146e35abcf59fd895f9eb3d275f1f422466
Ji Young Do <dojiyoung1844@gmail.com>
2023-08-13 01:10:58 +1200

    trying diabling database

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 // app.js
 
 const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+// const connectDB = require('./config/db');
+// const cors = require('cors');
 
 // routes
-const books = require('./routes/api/books');
+// const books = require('./routes/api/books');
 
 const app = express();
 
 // Connect Database
-connectDB();
+// connectDB();
 
 // cors
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
-app.use('/api/books', books);
+// app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// Export the Express API
+module.exports = app;
