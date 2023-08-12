6c387cd30c0131195ce0100e788439da5f62193d
Ji Young Do <dojiyoung1844@gmail.com>
2023-08-13 01:18:27 +1200

    trying diabling database

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 
 // routes
-// const books = require('./routes/api/books');
+const books = require('./routes/api/books');
 
 const app = express();
 
