ea489dc0b42d28a8687bc1766c39a73f0c0feea3
Ji Young Do <dojiyoung1844@gmail.com>
2023-08-13 01:58:41 +1200

    trying diabling database

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // app.js
 const express = require('express');
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 const cors = require('cors');
 
 // routes
-const books = require('./routes/api/books');
+// const books = require('./routes/api/books');
 
 const app = express();
 
 // Connect Database
-connectDB();
+// connectDB();
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
@@ -20,7 +20,7 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
-app.use('/api/books', books);
+// app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
